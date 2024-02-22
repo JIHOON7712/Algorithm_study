ca0c348e491027d6c92fe8f64ed42a2b8ac3aec0
JIHOON7712 <jihoon7712@gmail.com>
2024-02-22 09:21:06 +0900

    20240222

--- codingtest_prac/smart_factor.py
+++ codingtest_prac/smart_factor.py
@@ -1,41 +1,19 @@
 import sys
-n,k = map(int,sys.stdin.readline().split());
-count = 0
 
-inp = list(sys.stdin.readline())
-product_cnt=inp.count('H')
-print(product_cnt)
-print(len(inp))
-distance = k
-min_H=10
-flag=0
+N, K = map(int, sys.stdin.readline().split())
+PH_line = list(sys.stdin.readline().rstrip())
+cnt = 0
 
-
-while(inp.count(0) != product_cnt):
-    min_H=10
-    for i in range(len(inp)-1):
-        if inp[i] != 'P' and inp[i] != 0 and inp[i] != 'F':
-            cnt=0
-            for j in range(i-k,i+k+1):
-                if i==j or j<0 or j>=len(inp):
-                    continue;
-                else:
-                    if inp[j] == 'P':
-                        cnt+=1
-            inp[i] = cnt
-            if(inp[i] !=0) :
-                if(min_H>inp[i]):
-                    min_H = inp[i]
-    
-    for i2 in range(len(inp)):
-        if(inp[i2] == min_H):
-            for j2 in range(i2-k,i2+k+1):
-                if i2==j2 or j2<0 or j2>=len(inp):
-                    continue;
-                else:
-                    if inp[j2] == 'P':
-                        inp[j2] = 'F'
-                        inp[i2] = 0
-                        count +=1
-    print(inp,'\n')
-    print(count)
+for i in range(N):
+    if PH_line[i] == "P":  # 로봇일 경우에만 작동
+        for j in range(-K+i,K+i+1): # 로봇이 부품을 집을 수 있는 범위 안에서 왼쪽 부터 차례대로 탐색
+            if j < 0 or j > N-1: # 범위를 벗어나면 IndexError가 뜨기 때문에 지나감
+                continue
+            
+            # 부품을 집을경우 집었다는 처리를 하고 카운트를 센 후 다음 로봇으로 넘어간다
+            elif PH_line[j] == "H": 
+                PH_line[j] = None 
+                cnt += 1
+                break
+ 
+print(cnt)
