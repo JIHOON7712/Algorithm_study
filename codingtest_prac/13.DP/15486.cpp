be8921d09d9e185d1dbdedac971f2dda60b7b4d5
jihoon7712 <jihoon7712@gmail.com>
2024-05-20 02:58:44 +0900

    20240519

--- codingtest_prac/13.DP/15486.cpp
+++ codingtest_prac/13.DP/15486.cpp
@@ -1,20 +1,37 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
+
+#define MAX 15000002
 
 vector<pair<int,int>> vec;
 int day,pay;
 
-bool visited[1500001]={0,};
+int dp[MAX]={0,};
 int main(void){
     int N;
     cin >> N;
+
+    vec.push_back({0,0});
 
     for(int i =0;i<N;i++){
         cin >> day >> pay;
         vec.push_back({day,pay});
     }
 
-    for()
+    int curmax =0;
+
+    for(int j = 1;j<=N;j++){
+        curmax = max(curmax, dp[j]);
+        if(j+vec[j].first<=N+1){
+            dp[(j)+vec[j].first] = max(dp[(j)+vec[j].first],curmax+vec[j].second);
+        }
+        else{
+            dp[N+1] = max(dp[N+1],curmax);
+        }
+    }  
+
+    cout << dp[N+1] << endl;
 }
